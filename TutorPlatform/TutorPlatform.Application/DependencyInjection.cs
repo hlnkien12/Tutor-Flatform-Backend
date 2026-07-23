b94ca489cee57e868c25a4f46f62364dd7b0a66a
KienPC <kanetrinhhightlight19@gmail.com>
2026-07-23 14:05:36 +0700

    setup: Create Application layer with CQRS and validation configurations

---
+++ TutorPlatform/TutorPlatform.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Mapster;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using TutorPlatform.Application.Common.Behaviors;
+
+namespace TutorPlatform.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(assembly);
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            TypeAdapterConfig.GlobalSettings.Scan(assembly);
+
+            return services;
+        }
+    }
+}
