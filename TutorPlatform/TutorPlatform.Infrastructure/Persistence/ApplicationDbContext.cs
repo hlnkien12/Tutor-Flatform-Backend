f5163ada74f00ca62c34a0f8378a0ecd4c811b80
KienPC <kanetrinhhightlight19@gmail.com>
2026-07-23 13:32:19 +0700

    setup: Create Infrastructure layer with EF Core, DbContext and project reference

---
+++ TutorPlatform/TutorPlatform.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TutorPlatform.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
