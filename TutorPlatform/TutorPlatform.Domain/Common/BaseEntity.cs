237e5cdb772b93bdd4680aec258a5c057ff36f1c
KienPC <kanetrinhhightlight19@gmail.com>
2026-07-23 10:38:00 +0700

    feat: Setup Domain layer with base classes and project structure

---
+++ TutorPlatform/TutorPlatform.Domain/Common/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TutorPlatform.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime? UpdatedAt { get; protected set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        protected BaseEntity(Guid id, DateTime createdAt, DateTime? updatedAt)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        protected void MarkUpdated()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
