0ca6999f423431f649549a647311f945454e3b60
KienPC <kanetrinhhightlight19@gmail.com>
2026-07-23 14:38:09 +0700

    setup: Create API layer with Swagger, JWT configuration and WebAPI setup

---
+++ TutorPlatform/TutorPlatform.API/Common/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TutorPlatform.API.Common
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public int StatusCode { get; set; }
+        public T? Data { get; set; }
+        public List<string> Messages { get; set; } = new List<string>();
+
+        public static ApiResponse<T> Ok(T data)
+        {
+            return new ApiResponse<T> { Success = true, StatusCode = 200, Data = data };
+        }
+
+        public static ApiResponse<T> Created(T data)
+        {
+            return new ApiResponse<T> { Success = true, StatusCode = 201, Data = data };
+        }
+
+        public static ApiResponse<T> Error(int statusCode, params string[] messages)
+        {
+            return new ApiResponse<T> { Success = false, StatusCode = statusCode, Messages = new List<string>(messages) };
+        }
+    }
+}
